813102ef05b7e361d42e748bba1b5a770f7bf94d
Marie Duffy <meduffy@sdsu.edu>
2021-03-17 12:56:25 -0700

    Added sprite to Level 1 scene and added jumping animation and movement script to it

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class SpriteController : MonoBehaviour
 {
+    public float speed = 1.0f;
+    public Vector3 movement;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public Animator anim; 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            anim = GameObject.Find("Astronaut").GetComponent<Animator>();
+            //anim.speed = 1;
+        }
         
+        // movement = Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        // transform.position += movement * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed = 4.0f;
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
     }
 }
