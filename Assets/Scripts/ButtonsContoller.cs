bb980e9efa61b06ed85a610180c277531cceb4d3
Marie Duffy <meduffy@sdsu.edu>
2021-03-18 11:27:01 -0700

    Designed the credits page and made the necessary buttons for navigation

---
+++ Assets/Scripts/ButtonsContoller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsContoller : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadWelcomePage()
+    {
+        SceneManager.LoadScene("WelcomePage");
+    }
+}
