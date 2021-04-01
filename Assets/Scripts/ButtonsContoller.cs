49947a6cf3dbb7b3b1197881dc433d0c9edcc41d
Marie Duffy <meduffy@sdsu.edu>
2021-04-01 15:11:13 -0700

    Fixed code for new game/resume game buttons so that they work & go to the appropriate level if applicable

--- Assets/Scripts/ButtonsContoller.cs
+++ Assets/Scripts/ButtonsContoller.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonsContoller : MonoBehaviour
 {
+    public TMP_Text TextField;
+    public GameObject button;
+
+    public void Start()
+    {
+        //button = GameObject.Find("ProceedButton");
+        button.SetActive(false);
+    }
     public void LoadCredits()
     {
         SceneManager.LoadScene("Credits");
@@ -16,8 +25,40 @@ public class ButtonsContoller : MonoBehaviour
         SceneManager.LoadScene("WelcomePage");
     }
 
-    public void PlayGame()
+    public void PlayNewGame()
+    {
+        if (PlayerManager._instance.savedSceneIndex <= 1)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else
+        {
+            TextField.text = "You have a game in progress. Do you still want" +
+                             "to start a new game?";
+            button.SetActive(true);
+        }
+    }
+
+    public void NewGame()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void ResumeGame()
+    {
+        if (PlayerManager._instance.savedSceneIndex > 1)
+        {
+            SceneManager.LoadScene(PlayerManager._instance.savedSceneIndex);
+        }
+        else
+        {
+            TextField.text = "You do not have a game saved";
+        }
+    }
+
+    public void MainMenuSave()
+    {
+        PlayerManager._instance.savedSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("WelcomePage");
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int savedSceneIndex = 0;
+    public static PlayerManager _instance;
+    private void Awake()
+    {
+        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            _instance = this;
+            //player = new Player();
+
+        }
+    }
+}
