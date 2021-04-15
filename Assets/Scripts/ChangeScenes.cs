0acfe0a5f42c7640cc9dcb4c8b669de03bda7e85
Marie Duffy <meduffy@sdsu.edu>
2021-04-15 13:35:57 -0700

    Created a new sound and attached it to the Star object to play at the start of each level

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -5,13 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
-    //public string newLevel;
-
+    
     public void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
             SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+            //Invoke("LoadNextScene", 1.0f);
         }
+    }
+
+    private void LoadNextScene()
+    {
+        
     }
 }
---
+++ Assets/Scripts/PlayStarSound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayStarSound : MonoBehaviour
+{
+    // private AudioSource source;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     source = GetComponent<AudioSource>();
+    // }
+    //
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         source.Play();
+    //     }
+    // }
+}
